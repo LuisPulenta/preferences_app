2348dc0123ec59398de06aede88588371233ec49
LuisPulenta <luisalbertonu@gmail.com>
2024-06-05 18:42:38 -0300

    Cambios

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var a = 1;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
